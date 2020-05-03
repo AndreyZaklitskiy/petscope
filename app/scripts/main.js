1033f1d82e50a152c59742eb9781cec14ddf206c
AndreyZaklitskiy <glockman94@gmail.com>
2020-05-03 21:13:39 +0300

    instructions section is done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,24 @@
 $(function () {
-let headerList = $('.header-list');
+  let headerList = $('.header-list');
 
   $('.hamburger').on('click', function () {
     $(this).toggleClass('is-active');
-    if($(this).hasClass('is-active')){
-      headerList.animate({top : '0', right: '0'});
-    }else {
+    if ($(this).hasClass('is-active')) {
+      headerList.animate({top: '0', right: '0'});
+    } else {
       // $('.header-list').animate().removeClass('active');
-      headerList.animate({top : '-100vw', right: '-100vh'});
+      headerList.animate({top: '-100vw', right: '-100vh'});
     }
   });
-  $(window).resize(function() {
+  $(window).resize(function () {
     if ($(window).width() <= '992') {
       headerList.append($('.phone'));
-    }else if ($(window).width() > '992') {
+      $('.instructions-list-block > h2').after($('.instructions-image'))
+    } else if ($(window).width() > '992') {
       $('.header-btn-wrap').before($('.phone'));
+      $('.instructions>.container>.row').prepend($('.instructions-image'))
     }
-    })
+  });
 });
+
+// .instructions-image
