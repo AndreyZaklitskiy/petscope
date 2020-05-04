62f86c5eea240f76bafbaca4e77ed207e6b6d4a4
AndreyZaklitskiy <glockman94@gmail.com>
2020-05-04 16:57:09 +0300

    project: about_collar, buy_agitation sections done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,10 +13,14 @@ $(function () {
   $(window).resize(function () {
     if ($(window).width() <= '992') {
       headerList.append($('.phone'));
-      $('.instructions-list-block > h2').after($('.instructions-image'))
+      $('.instructions-list-block > h2').after($('.instructions-image'));
+      $('.buy_agitation-text-even > h2').after($('.buy_agitation-image-even'));
+      $('.buy_agitation-text-odd > h2').after($('.buy_agitation-image-odd'));
     } else if ($(window).width() > '992') {
       $('.header-btn-wrap').before($('.phone'));
-      $('.instructions>.container>.row').prepend($('.instructions-image'))
+      $('.instructions>.container>.row').prepend($('.instructions-image'));
+      $('.buy_agitation>.container>.row:even').prepend($('.buy_agitation-image-even'));
+      $('.buy_agitation>.container>.row:odd').append($('.buy_agitation-image-odd'));
     }
   });
 });
